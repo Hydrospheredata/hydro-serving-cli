ff1cc729aef411986215921f2ff546d1cb272561
Bulat <lb6557@gmail.com>
2018-03-06 15:02:39 +0300

    Feature/http client (#3)
    * Optimize imports and add package metadata
    
    * Http client lib for Hydroserving
    
    * Client

--- hydroserving/models/context_object.py
+++ hydroserving/models/context_object.py
@@ -1,4 +1,5 @@
 class ContextObject:
-    def __init__(self, metadata=None, docker_client=None):
+    def __init__(self, metadata=None, docker_client=None, kafka_params=None):
         self.metadata = metadata
         self.docker_client = docker_client
+        self.kafka_params = kafka_params
---
+++ hydroserving/models/kafka_params.py
@@ -0,0 +1,4 @@
+class KafkaParams:
+    def __init__(self, kafka, topic):
+        self.kafka = kafka
+        self.topic = topic
