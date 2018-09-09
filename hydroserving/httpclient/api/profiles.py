6fba5f64ff3f23c227ff3e963566fa1307cbb32c
Konstantin Makarychev <mrsimpson@inbox.ru>
2018-09-09 20:06:35 +0300

    Add push command to upload training data to data profiler

---
+++ hydroserving/httpclient/api/profiles.py
@@ -0,0 +1,20 @@
+from hydroserving.httpclient.errors import HSApiError
+
+
+class ProfilesAPI:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def push(self, model_version_id, filename, create_encoder_callback=None):
+        uid = self.connection.multipart_post(
+            url="/profiler/batch/{}/csv".format(model_version_id),
+            data = {},
+            files={"csv": ("filename", open(filename, "rb"), "application/octet-stream")},
+            create_encoder_callback=create_encoder_callback
+        )
+        return uid
+
+    def status(self, uid):
+        return self.connection.get(
+            url="/profiler/batch/{}/status".format(uid)
+        )
