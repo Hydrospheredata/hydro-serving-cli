02e2162cc8a5ff8622ef66af97b48d5e47972b50
Bulat <lb6557@gmail.com>
2018-09-10 17:53:22 +0300

    New jenkinsfile

---
+++ jenkinsfile.groovy
@@ -0,0 +1,34 @@
+def repository = 'hydro-serving-cli'
+
+
+def buildAndPublishReleaseFunction={
+    sh "sudo pip3 install --upgrade pip"
+    sh "sudo pip3 install setuptools==39.0.1"
+    sh "make PYTHON=python3 wheel"
+
+    def curVersion = getVersion()
+
+    sh "make PYTHON=python3 test"
+
+    sh 'sudo pip3 install twine'
+    configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: '.pypirc', variable: 'PYPI_SETTINGS')]) {
+        sh "twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*"
+    }
+}
+
+def buildFunction={
+    sh "sudo pip3 install --upgrade pip"
+    sh "sudo pip3 install setuptools==39.0.1"
+    sh "make PYTHON=python3 wheel"
+    sh "make PYTHON=python3 test"
+}
+
+pipelineCommon(
+        repository,
+        false, //needSonarQualityGate,
+        [],
+        {},//collectTestResults, do nothing
+        buildAndPublishReleaseFunction,
+        buildFunction,
+        buildFunction
+)
