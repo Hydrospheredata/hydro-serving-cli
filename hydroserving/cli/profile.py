003857a6f98a7d8e6790c2da5b79ef0dbe912935
Bulat <lb6557@gmail.com>
2018-09-12 18:11:35 +0300

    fix profile push

--- hydroserving/cli/profile.py
+++ hydroserving/cli/profile.py
@@ -1,37 +1,33 @@
-import click
-from hydroserving.cli.hs import hs_cli
-from hydroserving.constants.help import PROFILE_HELP, PROFILE_PUSH_HELP, PROFILE_MODEL_VERSION_HELP, CONTEXT_SETTINGS, UPLOAD_HOST_HELP, UPLOAD_PORT_HELP
-from hydroserving.httpclient.remote_connection import RemoteConnection
-from hydroserving.httpclient.api import ProfilesAPI, ModelAPI
-from hydroserving.helpers.upload import create_bar_callback_factory
 import time
+
+import click
+
+from hydroserving.cli import ensure_cluster
+from hydroserving.cli.hs import hs_cli
+from hydroserving.constants.help import PROFILE_HELP, PROFILE_PUSH_HELP, PROFILE_MODEL_VERSION_HELP, CONTEXT_SETTINGS
+from hydroserving.helpers.upload import create_bar_callback_factory
+from hydroserving.httpclient.api import ProfilesAPI, ModelAPI
+from hydroserving.httpclient.remote_connection import RemoteConnection
 
 
 @hs_cli.group(help=PROFILE_HELP)
-@click.option('--host',
-              default="localhost",
-              show_default=True,
-              help=UPLOAD_HOST_HELP,
-              required=False)
-@click.option('--port',
-              default=80,
-              show_default=True,
-              help=UPLOAD_PORT_HELP,
-              required=False)
-@click.pass_context
-def profile(ctx, host, port):
-    ctx.obj = {"host": host, "port": port}
+def profile():
+    pass
+
 
 @profile.command(help=PROFILE_PUSH_HELP, context_settings=CONTEXT_SETTINGS)
 @click.option('--model-version',
-             required=True,
-             help=PROFILE_MODEL_VERSION_HELP)
+              required=True,
+              help=PROFILE_MODEL_VERSION_HELP)
 @click.argument('filename', type=click.Path(exists=True))
 @click.pass_obj
 def push(obj, model_version, filename):
-    remote = RemoteConnection("http://{}:{}".format(obj["host"], obj["port"]))
+    current_cluster = ensure_cluster(obj)
+    click.echo("Using '{}' cluster".format(current_cluster['name']))
+    url = current_cluster['cluster']['server']
+    remote = RemoteConnection(url)
     profile_api = ProfilesAPI(remote)
-    if (model_version == "~~~"): # only for debugging
+    if model_version == "~~~":  # only for debugging
         mv = {"model": {"id": 1}, "modelVersion": 1}
         mv_id = 1
     else:
@@ -44,7 +40,8 @@ def push(obj, model_version, filename):
         click.echo()
         click.echo("Data profile computing is started with id {}".format(uid))
         status = ""
-        while (status != "success"):
+        while status != "success":
             status = profile_api.status(uid)
             time.sleep(30)
-        click.echo("Data profile for {} is ready: http://{}:{}/models/{}/{}".format(model_version, obj["host"], obj["port"], mv["model"]["id"], mv["modelVersion"]))
+        click.echo("Data profile for {} is ready: {}/models/{}/{}".format(
+            model_version, url, mv["model"]["id"], mv["modelVersion"]))
