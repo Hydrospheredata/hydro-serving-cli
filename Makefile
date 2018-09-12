1a97ed6035425e2e72460cef183b6c30fea8ea2b
Bulat <lb6557@gmail.com>
2018-09-12 11:24:20 +0300

    fix deps

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ PYTHON=python
 wheel:
 	$(PYTHON) setup.py bdist_wheel
 
-test: lint
+test:
 	export LC_ALL=en_us.UTF-8
 	export LANG=en_us.UTF-8
 	$(PYTHON) setup.py test
--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -21,6 +21,7 @@ def buildFunction={
     sh "sudo pip3 install setuptools==39.0.1"
     sh "make PYTHON=python3 wheel"
     sh "make PYTHON=python3 test"
+    sh "make PYTHON=python3 lint"
 }
 
 def collectTestResults = {
