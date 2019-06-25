6703544e738007d526d0de58e568a11af552c4bc
Konstantin Makarychev <mrsimpson@inbox.ru>
2019-06-25 13:04:27 +0300

    Fix hs apply for host selectors

--- hydroserving/core/host_selector/parser.py
+++ hydroserving/core/host_selector/parser.py
@@ -6,5 +6,5 @@ def parse_host_selector(in_dict):
         return None
     return HostSelector(
         name=in_dict.get("name"),
-        node_selector=in_dict.get("nodeSelector")
+        node_selector=in_dict.get("node_selector")
     )
