a13c4a99eda865e3078b1b401e915a105d14b812
Bulat Lutfullin <lb6557@gmail.com>
2020-03-26 14:01:14 +0300

    change release job

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,17 +2,18 @@ def repository = 'hydro-serving-cli'
 
 
 def buildAndPublishReleaseFunction={
-    sh "sudo pip3 install --upgrade pip"
-    sh "sudo pip3 install setuptools==39.0.1"
-    sh "make PYTHON=python3 wheel"
-
-    def curVersion = getVersion()
-
-    sh "make PYTHON=python3 test"
-
-    sh 'sudo pip3 install twine'
     configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: '.pypirc', variable: 'PYPI_SETTINGS')]) {
-        sh "twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*"
+        sh """#!/bin/bash
+        python3 -m venv venv &&
+        source venv/bin/activate
+        
+        pip install wheel~=0.34.2 &&
+        pip install setuptools==39.0.1 &&
+        pip install twine &&
+        make PYTHON=python wheel &&
+        make PYTHON=python test &&
+        twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*
+        """
     }
 }
 
